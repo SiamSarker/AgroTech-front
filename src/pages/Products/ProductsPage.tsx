c53497627b46c86bc105d6c29be4873e2b4b9c34
siamsarker <sarkersiam2@gmail.com>
2023-12-08 00:03:59 +0600

    final design from user module

--- src/pages/Products/ProductsPage.tsx
+++ src/pages/Products/ProductsPage.tsx
@@ -13,13 +13,10 @@ import productsData from "/Users/siamsarker/Documents/projects/AgroTech-front/as
 
 const defaultUser = {
   username: "Default User",
-  email: "",
   role: "default",
-  phone: "1234",
-  address: "Dhaka",
 };
 
-const ProductsPage = (props: any) => {
+const ProductsPage = () => {
   const [products, setProducts] = useState(productsData);
   const [user, setUser] = useState(defaultUser);
 
@@ -32,10 +29,7 @@ const ProductsPage = (props: any) => {
           setUser(userData);
         }
       } catch (error: any) {
-        console.error(
-          "Error fetching user data from AsyncStorage:",
-          error.message
-        );
+        console.error("Error fetching user data:", error.message);
       }
     };
 
@@ -44,47 +38,28 @@ const ProductsPage = (props: any) => {
 
   return (
     <ScrollView style={styles.container} stickyHeaderIndices={[0]}>
-      {/* Profile Section */}
       <View style={styles.profileContainer}>
-        {/* Profile Picture */}
         <Image
           source={{
-            uri:
-              "https://miro.medium.com/v2/resize:fit:2400/2*2TXYxlwIpt5W_5RgDvvT5w.jpeg",
+            uri: "https://miro.medium.com/v2/resize:fit:2400/2*2TXYxlwIpt5W_5RgDvvT5w.jpeg",
           }}
           style={styles.profileImage}
           resizeMode="cover"
         />
-
-        {/* User Information */}
         <View style={styles.userInfoContainer}>
           <Text style={styles.userName}>{user?.username}</Text>
           <Text style={styles.userType}>{user?.role}</Text>
         </View>
-
-        {/* Logo */}
-        <Image
-          source={{
-            uri:
-              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_pgM83QCuGt28zGxLoi1EtOKq0UfegvRdAvndqSJ-bkT-o3jlQ8KY414f0OCRxrj3PrE&usqp=CAU",
-          }}
-          style={styles.logo}
-          resizeMode="contain"
-        />
       </View>
 
-      {/* Products Section */}
       <View style={styles.productsContainer}>
         <Text style={styles.sectionTitle}>Products</Text>
-
-        {/* Product Boxes */}
         {products.map((product, index) => (
           <TouchableOpacity
             key={index}
             style={styles.productBox}
             onPress={() => console.log(`Product ${index + 1} pressed`)}
           >
-            {/* Product Image */}
             <View style={styles.productImageContainer}>
               <Image
                 source={{ uri: product.imageUri }}
@@ -92,8 +67,6 @@ const ProductsPage = (props: any) => {
                 resizeMode="cover"
               />
             </View>
-
-            {/* Product Information */}
             <View style={styles.productInfoContainer}>
               <Text style={styles.productInfoTextBold}>Name: {product.name}</Text>
               <Text>Available Quantity: {product.quantity}</Text>
@@ -103,9 +76,7 @@ const ProductsPage = (props: any) => {
               <Button
                 title="Add to Cart"
                 color="red"
-                onPress={() =>
-                  console.log(`Add to Cart pressed for Product ${index + 1}`)
-                }
+                onPress={() => console.log(`Add to Cart pressed for Product ${index + 1}`)}
               />
             </View>
           </TouchableOpacity>
@@ -118,75 +89,66 @@ const ProductsPage = (props: any) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f5f5f5", // Light gray background
+    backgroundColor: "#f5f5f5",
     padding: 20,
   },
   profileContainer: {
     flexDirection: "row",
-    justifyContent: "space-between",
     alignItems: "center",
-    marginTop: 20,
-    backgroundColor: "#ffffff", // White background
+    marginTop: 25,
+    backgroundColor: "#ffffff",
     padding: 10,
     borderBottomWidth: 1,
-    borderBottomColor: "#dddddd", // Light gray border
+    borderBottomColor: "#dddddd",
     shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 5,
-    },
+    shadowOffset: { width: 0, height: 3 },
     shadowOpacity: 0.36,
-    shadowRadius: 6.68,
-
-    elevation: 11,
+    shadowRadius: 3.68,
+    elevation: 3,
   },
   profileImage: {
     width: 50,
     height: 50,
     borderRadius: 25,
-  },
-  userInfoContainer: {
-    flex: 1,
     marginLeft: 10,
   },
+  userInfoContainer: {
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 10,
+    flex: 1,
+  },
   userName: {
-    color: "#333333", // Dark gray text
+    color: "#333333",
     fontWeight: "bold",
     fontSize: 16,
   },
   userType: {
-    color: "#666666", // Medium gray text
+    color: "#666666",
     fontSize: 14,
-  },
-  logo: {
-    width: 50,
-    height: 50,
   },
   productsContainer: {
     marginTop: 20,
   },
   sectionTitle: {
-    color: "#00cc00", // Green text
+    color: "#00cc00",
     fontWeight: "bold",
     fontSize: 20,
     marginBottom: 10,
   },
   productBox: {
     flexDirection: "row",
-    marginBottom: 20,
-    backgroundColor: "#ffffff", // White background
+    marginBottom: 5,
+    backgroundColor: "#ffffff",
     borderRadius: 10,
     borderWidth: 1,
-    borderColor: "#dddddd", // Light gray border
+    borderColor: "#dddddd",
     padding: 10,
     shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 5,
-    },
+    shadowOffset: { width: 0, height: 5 },
     shadowOpacity: 0.36,
-    shadowRadius: 6.68,
-    elevation: 11,
+    shadowRadius: 3.68,
+    elevation: 3,
   },
   productImageContainer: {
     flex: 4,
